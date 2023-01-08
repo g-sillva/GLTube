005e2e77c8d0f16ab0b00a60ab63090de8163342
g-sillva <silvagabriel.limars@gmail.com>
2023-01-08 16:43:08 -0300

    Added initial files

---
+++ gltube-backend/src/main/java/com/gabriel/gltube/GltubeApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.gltube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GltubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GltubeApplication.class, args);
+	}
+
+}
---
+++ gltube-backend/src/test/java/com/gabriel/gltube/GltubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.gltube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GltubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
