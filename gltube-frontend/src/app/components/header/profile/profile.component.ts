b4bbcbe6aa15faca0b456e93faab14ee0961f69b
g-sillva <silvagabriel.limars@gmail.com>
2023-01-14 19:07:38 -0300

    Frontend: Added some dynamic data insertion into header profile modal & changed it's features when user is not logged in

--- gltube-frontend/src/app/components/header/profile/profile.component.ts
+++ gltube-frontend/src/app/components/header/profile/profile.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  isProfileModalOpen = false;
+  isUserLogged = true;
+
+  user_username = "Mateus Andrade"
+  user_followers = 1200;
+  user_videos = 37;
+  user_channel_url = "";
+  user_profile_picture = 'assets/user-icon.png';
+
+  handleProfileClick() {
+    this.isProfileModalOpen = !this.isProfileModalOpen;
+  }
+
+  getImage() {
+    if (this.isUserLogged) {
+      this.user_profile_picture = "https://i.pinimg.com/236x/08/53/37/085337bd3b8103cb15cf3a8c0b61701e.jpg";
+    }
+    return this.user_profile_picture;
+  }
 }
