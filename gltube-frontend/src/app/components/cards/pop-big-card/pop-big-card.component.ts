a6b2e7a78705ae74fe8daf667cea4c114c309c31
g-sillva <silvagabriel.limars@gmail.com>
2023-01-17 09:40:09 -0300

    Frontend: Added bottom dark fade on big popular video card and implemented variable data with @Input()

--- gltube-frontend/src/app/components/cards/pop-big-card/pop-big-card.component.ts
+++ gltube-frontend/src/app/components/cards/pop-big-card/pop-big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pop-big-card-component',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./pop-big-card.component.scss']
 })
 export class PopBigCardComponent {
+
+  @Input() url = "/";
+
+  @Input() thumbnail = "";
+  
+  @Input() title = "Video title...";
+  
+  @Input() author_profile_image = "assets/user-icon.png";
+  
+  @Input() author_username = "Author...";
+  
+  @Input() views: number = 0;
+  
+  @Input() date = "now";
+  
+  @Input() length = "0:00";
 
 }
--- gltube-frontend/src/app/components/popular-videos/popular-videos.component.ts
+++ gltube-frontend/src/app/components/popular-videos/popular-videos.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class PopularVideosComponent {
 
+  videos = [
+    {
+      isPrincipal: false,
+      url: "/",
+      thumbnail: "https://www.magneticmag.com/.image/t_share/MTg5MzAxOTU3ODM0MzE5NzY1/thumb-1920-1222761.jpg",
+      title: "Let's Play Elden Ring Seamless Coop - ME...",
+      author_profile_image: "https://i.pinimg.com/236x/08/53/37/085337bd3b8103cb15cf3a8c0b61701e.jpg",
+      author_username: "Mateus Andrade",
+      views: 2731,
+      date:  "1 month ago",
+      length: "12:24"
+    }
+  ]
+
 }
