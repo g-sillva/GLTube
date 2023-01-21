c84d22bee8a6afbaa18b7e562c9e8b7e18b2e836
g-sillva <silvagabriel.limars@gmail.com>
2023-01-20 21:12:24 -0300

    Frontend: added 'first card' modification on video suggestion cards

--- gltube-frontend/src/app/components/cards/video-sug-card/video-sug-card.component.ts
+++ gltube-frontend/src/app/components/cards/video-sug-card/video-sug-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'video-sug-card-component',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class VideoSugCardComponent {
 
+  @Input() isFirst = false;
 }
